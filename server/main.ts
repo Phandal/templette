e7be5150b732dad79aa8184882ffb61e4fc26090
Phandal <bfeld.111@gmail.com>
2025-04-30 14:36:06 -0500

    use vite

---
+++ server/main.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+import ViteExpress from 'vite-express';
+
+const PORT = 3000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+ViteExpress.listen(app, PORT, () => {
+  console.log(`Server is listening on port localhost:${PORT}...`);
+});
