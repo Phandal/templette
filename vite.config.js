6be3d9c535720ed37fe91741af0b43dc01490b25
Phandal <bfeld.111@gmail.com>
2025-05-02 14:26:16 -0500

    remove uneeded server to make this fully static

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
-import { dirname, resolve } from 'node:path';
-import { fileURLToPath } from 'node:url';
+// import { dirname, resolve } from 'node:path';
+// import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+// const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [cssInjectedByJsPlugin],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     input: {
+  //       main: resolve(__dirname, 'index.html'),
+  //     },
+  //   },
+  // },
   optimizeDeps: {
     include: ['monaco-editor'],
   },
